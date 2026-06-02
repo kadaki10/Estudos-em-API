881e5ac453783b197add8a460ba641d37db78501
Reinaldo/python <reinaldo_997@hotmail.com>
2026-06-02 20:57:30 -0300

    Implementando Modelo Abstrato

---
+++ src/models/abstract_model.py
@@ -0,0 +1,44 @@
+# abstract_model.py
+from pymongo.collection import ReturnDocument, Collection
+
+
+class AbstractModel:
+    _collection: Collection = None
+
+    def __init__(self, data: dict):
+        self.data = data
+
+    def save(self):
+        result = self._collection.insert_one(self.data)
+        insert_document = self._collection.find_one(
+            {"_id": result.inserted_id}
+        )
+        self.data = inserted_document
+        return self.data
+        
+    def update(self, data:dict):
+        # Por padrão o find_one_and_update retorna o estado anterior do 
+        # Registrom, por isso, utilizamos o ReturnDocument.AFTER para
+        # Retornar a versão pos atualização
+        result = self._collection.find_one_and_update(
+                {"_id": self.data["_id"]}, 
+                {"$set": data},
+                return_document = ReturnDocument.AFTER,
+        )
+
+        self.data = result
+        return self.data
+        
+    def delete(self):
+        self._collection.delete_one({"_id": self.data["_id"]})
+
+    @classmethod
+    def find(cls, query: dict = {}):
+        data = cls._collection.find(query)
+        return [cls(d) for d in data]
+        
+    @classmethod
+    def find_one(cls, query: dict):
+        data = cls._collection.find_one(query)
+        return cls(data) if data else None
+            
